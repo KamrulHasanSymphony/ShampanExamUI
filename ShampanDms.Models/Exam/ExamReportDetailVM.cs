227e1e46edfa353a091132bcf046b8ebda3f2923
RakibulTusarSSL <rakibul.tusar@symphonysoftt.com>
2025-12-04 15:43:30 +0600

    Exam Result Report Added.

---
+++ ShampanDms.Models/Exam/ExamReportDetailVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShampanExam.Models.Exam
+{
+    public class ExamReportDetailVM
+    {
+        public int? Id { get; set; }
+        public string? ExamineeName { get; set; }
+        public string? ExamineeMobileNo { get; set; }
+        public string? MarkObtain { get; set; }
+        public string? Grade { get; set; }
+        public string? TotalMark { get; set; }
+        public string? Percentage { get; set; }
+    }
+}
